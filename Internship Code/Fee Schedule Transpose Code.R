bed2db3a5f146a538c457e8c969a1074f6dc305b
Zara Clacken <81605744+zclacken@users.noreply.github.com>
2022-08-10 11:27:38 -0400

    Update Fee Schedule Transpose Code.R

--- Internship Code/Fee Schedule Transpose Code.R
+++ Internship Code/Fee Schedule Transpose Code.R
@@ -1,9 +1,13 @@
-fee_schedule = Enteral_K0553_K0554_Cleaned_Fee_Schedule
+#Import cleaned fee schedule file 
+fee_schedule = 
+#Count the number of columns in the fee schedule
 ncol(fee_schedule)
-HCPCS = rep(fee_schedule$Hcpcs,92)
-MOD1 = rep(fee_schedule$Mod1,92)
-MOD2 = rep(fee_schedule$Mod2,92)
-Rate_Plan_Stacked = stack(fee_schedule, select = 7:98)
+#n is the number of plan/state columns  
+HCPCS = rep(fee_schedule$Hcpcs,n)
+MOD1 = rep(fee_schedule$Mod1,n)
+MOD2 = rep(fee_schedule$Mod2,n)
+#x is the index number of the first plan/state column; y is the index number of the final plan/state column 
+Rate_Plan_Stacked = stack(fee_schedule, select = x:y)
 Plan = Rate_Plan_Stacked$ind  
 Rates = Rate_Plan_Stacked$values
 transposed_df = data.frame(HCPCS,MOD1,MOD2,Plan,Rates)
